8cd8ae6f629978b5d913620f0c05a6b4f45f9c5b
luistovar <luistovar@cmg.mx>
2026-07-13 12:44:10 -0600

    .

--- ReadText.cs
+++ ReadText.cs
@@ -7,10 +7,10 @@ using System.Collections.Generic;
 
 namespace VisionDemo
 {
-    class Programm
+    class Program
     {
         // 👉 Reemplaza con tu endpoint y key del portal de Azure
-        private static string endpoint = "";
+        private static string endpoint = "https://pruebascv.cognitiveservices.azure.com/";
         private static string key = "";
 
         static async Task Main(string[] args)
@@ -47,13 +47,100 @@ namespace VisionDemo
 
                 if (results.Status == OperationStatusCodes.Succeeded)
                 {
+                    string nombre = "";
+                    string sexo = "";
+                    string domicilio = "";
+                    string claveDeElector = "";
+                    string curp = "";
+                    string vigencia = "";
+                    string fechaNacimiento = "";
+                    string seccion = "";
+                    string anioRegistro = "";
+
+                    int nombreCount = 0;
+                    int domicilioCount = 0;
+
                     foreach (var page in results.AnalyzeResult.ReadResults)
                     {
                         foreach (var line in page.Lines)
                         {
-                            Console.WriteLine(line.Text);
+                            if (line.Text == "INE")
+                                continue;
+                            if (line.Text == "LINE")
+                                continue;
+                            if (line.Text == "INSTITUTO NACIONAL ELECTORAL")
+                                continue;
+                            if (line.Text == "MÉXICO")
+                                continue;
+
+                            if (line.Text == "SEXO M")
+                                sexo = "Mujer";
+                            else if (line.Text == "SEXO F")
+                                sexo = "Hombre";
+                            else if (line.Text == "NOMBRE")
+                            {
+                                nombreCount = 1;
+                            }
+                            else if (nombreCount == 1)
+                            {
+                                nombreCount = 2;
+                                nombre += " " + line.Text;
+                            }
+                            else if (nombreCount == 2)
+                            {
+                                nombreCount = 3;
+                                nombre += " " + line.Text;
+                            }
+                            else if (nombreCount == 3)
+                            {
+                                nombreCount = 4;
+                                nombre += " " + line.Text;
+                            }
+
+                            if (line.Text == "DOMICILIO")
+                            {
+                                domicilioCount = 1;
+                            }
+                            else if (domicilioCount == 1)
+                            {
+                                if (!line.Text.ToLower().Contains("clave"))
+                                    domicilio = domicilio + " " + line.Text;
+                                else
+                                    domicilioCount = 2;
+                            }
+
+                            if (line.Text.ToLower().Contains("clave"))
+                                claveDeElector = line.Text.ToLower().Replace("clave de elector", "");
+
+                            else if (line.Text.Length == 18)
+                                curp = line.Text;
+
+                            else if (line.Text.Length == 11)
+                                vigencia = line.Text;
+
+                            else if (line.Text.Length == 10 && line.Text.Contains("/"))
+                                fechaNacimiento = line.Text;
+
+                            //validar si es numerico y longitud 4
+                            bool esValido = line.Text.Length == 4 && line.Text.All(char.IsDigit);
+
+                            if (esValido)
+                                seccion = line.Text;
+
+                            else if (line.Text.Length == 7 && line.Text.Contains(" "))
+                                anioRegistro = line.Text;
                         }
                     }
+
+                    Console.WriteLine("Nombre: " + nombre.Trim());
+                    Console.WriteLine("Sexo: " + sexo);
+                    Console.WriteLine("Domicilio: " + domicilio.Trim());
+                    Console.WriteLine("Clave de elector: " + claveDeElector.Trim().ToUpper());
+                    Console.WriteLine("CURP: " + curp);
+                    Console.WriteLine("Año de registro: " + anioRegistro);
+                    Console.WriteLine("Fecha de nacimiento: " + fechaNacimiento);
+                    Console.WriteLine("Sección: " + seccion);
+                    Console.WriteLine("Vigencia: " + vigencia);
                 }
                 else
                 {
